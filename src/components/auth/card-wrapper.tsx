e05114fd2ba622b8781f10bc9ba77c3a63f452dd
Salman Farsi <sfjadi@gmail.com>
2024-04-27 16:15:40 -0400

    Update import order in card-wrapper.tsx

--- src/components/auth/card-wrapper.tsx
+++ src/components/auth/card-wrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { Card, CardHeader, CardContent, CardFooter } from "@/components/ui/card";
 import { Header } from "@/components/auth/header";
 import { Social } from "@/components/auth/social";
 import { BackButton } from "@/components/auth/back-button";
