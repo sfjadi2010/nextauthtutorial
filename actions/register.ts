4058a4e252fb1f9dd06919245c43506411a3d189
Salman Farsi <sfjadi@gmail.com>
2024-05-21 06:58:42 -0400

    feat: Add getUserByEmail function and use it in register action

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,7 @@ import * as zod from "zod";
 import { RegisterSchema } from "@/schemas";
 import bcrypt from "bcrypt";
 import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: zod.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -15,11 +16,7 @@ export const register = async (values: zod.infer<typeof RegisterSchema>) => {
   const { fname, lname, email, password } = validatedFields.data;
   const hashedPasswrod = await bcrypt.hash(password, 10);
 
-  const existingUser = await db.user.findUnique({
-    where: {
-      email,
-    },
-  });
+  const existingUser = await getUserByEmail(email);
 
   if (existingUser) {
     return { error: "Email already in use!" };
@@ -33,6 +30,6 @@ export const register = async (values: zod.infer<typeof RegisterSchema>) => {
         password: hashedPasswrod
     }
   });
-
-  return { success: "Email sent!!!" };
+  // TODO: Send verification email
+  return { success: "User created!" };
 };
